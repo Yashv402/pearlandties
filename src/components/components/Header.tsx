6a9492b5621c8fffd5a9da1526c0bf52c7ca8e36
Yashv402 <syashvi402@gmail.com>
2025-08-06 16:28:10 +0530

    left with tailwind error

--- src/components/components/Header.tsx
+++ src/components/components/Header.tsx
@@ -1,22 +1,37 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-  return (
-    <div>
-        <header className="flex justify-between p-4">
-            <h1 className="text-2xl font-bold">My Application</h1>
-            <nav>
-            <ul className="flex space-x-4">
-                <li><Link href="/" className="hover:underline">Home</Link></li>
-                <li><Link href="/about" className="hover:underline">About</Link></li>
-                <li><Link href="/contact" className="hover:underline">Contact</Link></li>
-            </ul>
-            </nav>
-            <span>profile</span>
-        </header>
+ return (
+  <div>
+   <header className="">
+    {/* Icon */}
+    <div className="w-10 h-10 rounded-full border">
+     <img src="src/app/favicon.ico" alt="Logo" />
     </div>
-  )
-}
+    <nav>
+     <ul className="flex space-x-4">
+      <li>
+       <Link href="/" className="hover:underline">
+        Home
+       </Link>
+      </li>
+      <li>
+       <Link href="/about" className="hover:underline">
+        About
+       </Link>
+      </li>
+      <li>
+       <Link href="/contact" className="hover:underline">
+        Contact
+       </Link>
+      </li>
+     </ul>
+    </nav>
+    <span>profile</span>
+   </header>
+  </div>
+ );
+};
 
-export default Header
+export default Header;
